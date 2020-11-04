83d59ffd8240e3f474f65161c0fcc161e82ebd54
msearles25 <skaman4459@gmail.com>
2020-11-04 13:23:12 -0500

    InputHandler class prototype

--- SpaceInvadersClone/src/Engine/GameEngine.cpp
+++ SpaceInvadersClone/src/Engine/GameEngine.cpp
@@ -7,7 +7,7 @@ GameEngine::GameEngine()
 	m_Window.create(sf::VideoMode(m_Resolution.x, m_Resolution.y), 
 		"Space Invaders++", sf::Style::Fullscreen);
 
-	m_ScreenManager = unique_ptr<ScreenManager>(new ScreenManager(
+	m_ScreenManager = std::unique_ptr<ScreenManager>(new ScreenManager(
 		sf::Vector2i(m_Resolution.x, m_Resolution.y)));
 }
 
---
+++ SpaceInvadersClone/src/Screens/InputHandler.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include "Button.h"
+#include "Screen.h"
+#include "ScreenManagerRemoteControl.h"
+
+class InputHandler
+{
+private:
+	Screen* m_ParentScreen;
+	std::vector<std::shared_ptr<Button>> m_Button;
+	sf::View* m_PointerToUIPanelView;
+	ScreenManagerRemoteControl* m_ScreenManagerRemoteControl;
+public:
+	void initializeInputHandler(
+		ScreenManagerRemoteControl* sw,
+		std::vector<std::shared_ptr<Button>> m_Buttons,
+		sf::View* pointerToView,
+		Screen* parentScreen);
+
+	void handleInput(sf::RenderWindow& window, sf::Event& event);
+
+	virtual void handleGamepad();
+	virtual void handleKeyPressed(sf::Event& event, sf::RenderWindow& window);
+
+	virtual void handleKeyReleased(sf::Event& event, sf::RenderWindow& window);
+
+	virtual void handleLeftClick(std::string& buttonInteractWidth, sf::RenderWindow& window);
+
+	sf::View* getPointerToUIView();
+
+	ScreenManagerRemoteControl* getPointerToScreenManagerRemoteControl();
+
+	Screen* getParentScreen();
+};
