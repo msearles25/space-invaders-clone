6b29ab54e0ad258801485615a171b5a818157579
msearles25 <skaman4459@gmail.com>
2020-11-04 15:46:07 -0500

    SelectScreen constructor

---
+++ SpaceInvadersClone/src/Screens/Select/SelectScreen.cpp
@@ -0,0 +1,21 @@
+#include "SelectScreen.h"
+#include "SelectInputHandler.h"
+#include "SelectUIPanel.h"
+
+
+SelectScreen::SelectScreen(ScreenManagerRemoteControl* smrc, sf::Vector2i res)
+{
+	auto suip{ std::make_unique<SelectUIPanel>(res) };
+	auto sih{ std::make_shared<SelectInputHandler>() };
+	addPanel(std::move(suip), smrc, sih);
+	m_ScreenManagerRemoteControl = smrc;
+
+	m_BackgroundTexture.loadFromFile("Content/graphics/background.png");
+
+	m_BackgroundSprite.setTexture(m_BackgroundTexture);
+	auto textureSize{ m_BackgroundSprite.getTexture()->getSize() };
+
+	m_BackgroundSprite.setScale(
+		float(m_View.getSize().x) / textureSize.x,
+		float(m_View.getSize().y) / textureSize.y);
+}
---
+++ SpaceInvadersClone/src/Screens/Select/SelectScreen.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "../Screen.h"
+
+class SelectScreen : public Screen
+{
+private:
+	ScreenManagerRemoteControl* m_ScreenManagerRemoteControl;
+
+	sf::Texture m_BackgroundTexture;
+	sf::Sprite m_BackgroundSprite;
+public:
+	SelectScreen(ScreenManagerRemoteControl* smrc, sf::Vector2i res);
+	virtual void draw(sf::RenderWindow& window);
+};
