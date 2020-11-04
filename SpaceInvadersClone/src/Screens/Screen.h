70911c2098ab9ccd3a2ec9087fa80acb94b8e825
msearles25 <skaman4459@gmail.com>
2020-11-04 15:00:07 -0500

    Screen class

---
+++ SpaceInvadersClone/src/Screens/Screen.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include "InputHandler.h"
+#include "ScreenManagerRemoteControl.h"
+#include "UIPanel.h"
+
+class Screen
+{
+private:
+	std::vector<std::shared_ptr<InputHandler>> m_InputHandlers;
+	std::vector<std::unique_ptr<UIPanel>> m_Panels;
+protected:
+	void addPanel(std::unique_ptr<UIPanel> p,
+		ScreenManagerRemoteControl* smrc,
+		std::shared_ptr<InputHandler> ih);
+public:
+	virtual void initialize();
+	virtual void update(float fps);
+	virtual void draw(sf::RenderWindow& window);
+	void handleInput(sf::RenderWindow& window);
+
+	sf::View m_View;
+};
