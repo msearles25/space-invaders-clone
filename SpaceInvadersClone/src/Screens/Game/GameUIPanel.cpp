a03aa6f6f6ffed38568bc4d6b9dbd49955bc4701
msearles25 <skaman4459@gmail.com>
2020-11-04 18:20:45 -0500

    GameUIPanel constructor

---
+++ SpaceInvadersClone/src/Screens/Game/GameUIPanel.cpp
@@ -0,0 +1,23 @@
+#include <sstream>
+#include "GameUIPanel.h"
+#include "../../Engine/WorldState.h"
+
+int WorldState::SCORE;
+int WorldState::LIVES;
+
+GameUIPanel::GameUIPanel(sf::Vector2i res) :
+	UIPanel(res,
+		1, // The left
+		1, // The top
+		res.x / 3, // 1/3 of the width of the screen
+		res.y / 12,
+		50, 255, 255, 255)
+{
+	m_Text.setFillColor(sf::Color(0, 255, 0, 255));
+	m_Text.setString("Score: 0 Lives: 3 Wave: 1");
+	m_Font.loadFromFile("Content/fonts/Roboto-Bold.ttf");
+	m_Text.setFont(m_Font);
+	m_Text.setPosition(sf::Vector2f(15, 15));
+	m_Text.setCharacterSize(60);
+}
+
---
+++ SpaceInvadersClone/src/Screens/Game/GameUIPanel.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../UIPanel.h"
+
+class GameUIPanel : public UIPanel
+{
+public:
+	GameUIPanel(sf::Vector2i res);
+	void draw(sf::RenderWindow& window) override;
+};
