66c126b9a67ef2031f6991584d5188d41d8114a0
msearles25 <skaman4459@gmail.com>
2020-11-05 10:29:34 -0500

    ColliderCompnent

---
+++ SpaceInvadersClone/src/GameObjects/ColliderComponent.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include "Component.h"
+
+class ColliderComponent : public Component
+{
+	std::string m_Type{ "collider" };
+	bool m_Enabled{ false };
+public:
+	/*
+***********************************************
+***********************************************
+From Component interface
+***********************************************
+***********************************************
+*/
+	std::string Component::getType()
+	{
+		return m_Type;
+	}
+
+	void Component::disableComponent()
+	{
+		m_Enabled = false;
+	}
+
+	void Component::enableComponent()
+	{
+		m_Enabled = true;
+	}
+
+	bool Component::enabled()
+	{
+		return m_Enabled;
+	}
+
+	void Component::start(GameObjectSharer* gos, GameObject* self)
+	{
+
+	}
+};
