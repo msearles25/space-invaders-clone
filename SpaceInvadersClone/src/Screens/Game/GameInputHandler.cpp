8362c413e213b328e9f7023628c009361a64f5b8
Searles <skaman4459@gmailcom>
2020-11-07 10:53:37 -0500

    Handling input from a gamepad

--- SpaceInvadersClone/src/Screens/Game/GameInputHandler.cpp
+++ SpaceInvadersClone/src/Screens/Game/GameInputHandler.cpp
@@ -6,7 +6,46 @@ class BulletSpawner;
 
 void GameInputHandler::initialize() {}
 
-void GameInputHandler::handleGamepad() {}
+void GameInputHandler::handleGamepad() 
+{
+	float deadZone{ 10.0f };
+	float x{ sf::Joystick::getAxisPosition(0, sf::Joystick::X) };
+	float y{ sf::Joystick::getAxisPosition(0, sf::Joystick::Y) };
+
+	if (x < deadZone && x > -deadZone)
+	{
+		x = 0;
+	}
+	if (y < deadZone && y > -deadZone)
+	{
+		y = 0;
+	}
+
+	m_PUC->updateShipTravelWithController(x, y);
+
+	// Has the player pressed the B/circle(ps) button
+	if (sf::Joystick::isButtonPressed(0, 1))
+	{
+		mBButtonPressed = true;
+	}
+
+	// Has the player released the B/circle(ps) button
+	if (!sf::Joystick::isButtonPressed(0, 1) && mBButtonPressed)
+	{
+		mBButtonPressed = false;
+
+		// Shoot a bullet
+		SoundEngine::playShoot();
+		sf::Vector2f spawnLocation;
+		
+		spawnLocation.x = m_PTC->getLocation().x + m_PTC->getSize().x / 2;
+
+		spawnLocation.y = m_PTC->getLocation().y;
+
+		static_cast<GameScreen*>(getParentScreen())->
+			getBulletSpawner()->spawnBullet(spawnLocation, true);
+	}
+}
 
 void GameInputHandler::handleKeyPressed(sf::Event& event, sf::RenderWindow& window)
 {
