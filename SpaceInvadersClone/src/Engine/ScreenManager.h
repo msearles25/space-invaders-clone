e1d73c6cda9ce50d9ff1a43dd16e448c35688d6a
msearles25 <skaman4459@gmail.com>
2020-11-06 21:19:15 -0500

    Udpated the ScreenManager to use the new LevelManager

--- SpaceInvadersClone/src/Engine/ScreenManager.h
+++ SpaceInvadersClone/src/Engine/ScreenManager.h
@@ -4,6 +4,7 @@
 #include <SFML/Graphics.hpp>
 #include "BitmapStore.h"
 #include "../Screens/Game/GameScreen.h"
+#include "../FileIO/LevelManager.h"
 #include "../Screens/ScreenManagerRemoteControl.h"
 #include "../Screens/Select/SelectScreen.h"
 
@@ -11,6 +12,7 @@ class ScreenManager : public ScreenManagerRemoteControl
 {
 private:
 	std::map<std::string, std::unique_ptr<Screen>> m_Screens;
+	LevelManager m_LevelManager;
 protected:
 	std::string m_CurrentScreen = "Select";
 public:
@@ -36,6 +38,18 @@ public:
 
 	void ScreenManagerRemoteControl::loadLevelInPlayMode(std::string screenToLoad)
 	{
+		m_LevelManager.getGameObjects().clear();
+		m_LevelManager.loadGameObjectsForPlayMode(screenToLoad);
 		SwitchScreens("Game");
+	}
+
+	std::vector<GameObject>& ScreenManagerRemoteControl::getGameObjects()
+	{
+		return m_LevelManager.getGameObjects();
+	}
+
+	GameObjectSharer& shareGameObjectSharer()
+	{
+		return m_LevelManager;
 	}
 };
--- SpaceInvadersClone/src/Screens/ScreenManagerRemoteControl.h
+++ SpaceInvadersClone/src/Screens/ScreenManagerRemoteControl.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "../GameObjects/GameObject.h"
+#include "../FileIO/GameObjectSharer.h"
 
 class ScreenManagerRemoteControl
 {
 public:
 	virtual void SwitchScreens(std::string screenToSwitch) = 0;
 	virtual void loadLevelInPlayMode(std::string screenToLoad) = 0;
+	virtual std::vector<GameObject>& getGameObjects() = 0;
+	virtual GameObjectSharer& shareGameObjectSharer() = 0;
 };
