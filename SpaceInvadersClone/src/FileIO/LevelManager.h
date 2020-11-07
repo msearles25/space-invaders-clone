f6d88437726c90dcea881d8bc7d1ea19dcd6623e
msearles25 <skaman4459@gmail.com>
2020-11-06 20:53:30 -0500

    LevelManager

---
+++ SpaceInvadersClone/src/FileIO/LevelManager.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "../GameObjects/GameObject.h"
+#include "GameObjectSharer.h"
+
+class LevelManager : public GameObjectSharer
+{
+private:
+	std::vector<GameObject> m_GameObjects;
+
+	const std::string WORLD_FOLDER{ "world" };
+	const std::string SLASH{ "/" };
+
+	void runStartPhase();
+	void activateAllGameObjects();
+public:
+	std::vector<GameObject>& getGameObjects();
+	void loadGameObjectsForPlayMode(std::string screenToLoad);
+
+	/***********************************************
+	***********************************************
+	From GameObjectSharer interface
+	***********************************************
+	***********************************************/
+
+	std::vector<GameObject>& GameObjectSharer::getGameObjectsWithGOS()
+	{
+		return m_GameObjects;
+	}
+	
+	GameObject& GameObjectSharer::findFirstObjectWithTag(std::string tag)
+	{
+		auto it{ m_GameObjects.begin() };
+		auto end{ m_GameObjects.end() };
+
+		for (it; it < end; ++it)
+		{
+			if ((*it).getTag() == tag)
+			{
+				return (*it);
+			}
+		}
+#ifdef debuggingErrors
+		std::cout << "LevelManager.h findFirstObjectWithTag() "
+			<< "- TAG NOT FOUND, ERROR!\n";
+#endif // debuggingErrors
+		return m_GameObjects[0];
+	}
+};
